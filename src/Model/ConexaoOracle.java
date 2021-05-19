73a3fc98d514626706a586f642fc86f267c6cb72
lsilva <lsilva@192.168.1.10>
2021-05-19 03:23:17 -0300

    OracleConnection_class

--- src/Model/ConexaoOracle.java
+++ src/Model/ConexaoOracle.java
@@ -5,15 +5,74 @@
  */
 package Model;
 
+import static Model.OracleConnection.DBPASS;
+import static Model.OracleConnection.DBURL;
+import static Model.OracleConnection.DBUSER;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author lsilva
  */
 public class ConexaoOracle {
-    private String dbUrl, dbUser,dbPass;
-    
-    
-    public ConectarOracle(String dbUrl, Sring dbUser, String dbPass){
-        return null;
+
+    private String dbUrl, dbUser, dbPass;
+    Connection con;
+
+    public ConexaoOracle(String dbUrl, String dbUser, String dbPass) {
+        this.dbUrl = dbUrl;
+        this.dbUser = dbUser;
+        this.dbPass = dbPass;
+    }
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public void setDbUrl(String dbUrl) {
+        this.dbUrl = dbUrl;
+    }
+
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public void setDbUser(String dbUser) {
+        this.dbUser = dbUser;
+    }
+
+    public String getDbPass() {
+        return dbPass;
+    }
+
+    public void setDbPass(String dbPass) {
+        this.dbPass = dbPass;
+    }
+
+    public boolean ConectarOracle(String dbUser, String dbPass, String dbUrl) throws SQLException {
+        DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        con = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT SYSDATE FROM DUAL");
+        if (rs.next()) {
+            Date currentDate = rs.getDate(1); // get first column returned
+            //System.out.println("Current Date from Oracle is : "+currentDate);
+            rs.close();
+            statement.close();
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public boolean DesconectarOracle() throws SQLException {
+        con.close();
+        return true;
     }
 }
