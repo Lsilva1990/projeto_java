fef343d2194d0ff1eb23c840ab13a646623f3af6
lsilva <lsilva@192.168.1.10>
2021-05-19 02:50:40 -0300

    OracleConnection_class

---
+++ src/Model/ConexaoOracle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author lsilva
+ */
+public class ConexaoOracle {
+    private String dbUrl, dbUser,dbPass;
+    
+    
+    public ConectarOracle(String dbUrl, Sring dbUser, String dbPass){
+        return null;
+    }
+}
